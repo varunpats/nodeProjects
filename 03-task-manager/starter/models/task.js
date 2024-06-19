859b01d2e8c89cd2ba70ec8c6b03a613b011b897
varunpats <varunpatil2502@gmail.com>
2024-06-19 22:11:56 +0530

    Added validation

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, "Provide name"],
+        trim: true,
+        maxlength: [20, 'Name must be less than 20 chars']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('tasks', taskSchema);
